e8c7a4fc38f144a63594a1eee1b8f8f02379ffcb
Waleed alfouzan <umelgharabi@gmail.com>
2019-03-21 14:00:33 +0300

    Initial check-in of module cms

---
+++ BethanyPieShop/Models/IPieRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+namespace BethanyPieShop.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> GetAllPies();
+        Pie GetPieById(int pieId);
+    }
+}
---
+++ BethanyPieShop/ViewModels/HomeViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using BethanyPieShop.Models;
+using System.Collections.Generic;
+
+namespace BethanyPieShop.ViewModels
+{
+    public class HomeViewModels
+    {
+        public string Title { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
